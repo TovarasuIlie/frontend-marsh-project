c9d7a8eefaa06375cd4d182460cbfec773cb2d42
Niculai Ilie-Traian <niculaii58@gmail.com>
2026-04-07 19:31:35 +0300

    add assigned user to device

--- src/app/models/device.ts
+++ src/app/models/device.ts
@@ -1,8 +1,11 @@
+import { User } from "./user"
+
 export interface Device {
     id: number,
     name: string,
     manufacturer: string,
     type: number,
+    assignedToUser?: User
     operatingSystem: string,
     osVersion: string,
     processor: string,
---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+import { UserRole } from "../enums/user-role";
+
+export interface User {
+    id: number,
+    name: string,
+    email: string,
+    role: UserRole,
+    location: string
+}
